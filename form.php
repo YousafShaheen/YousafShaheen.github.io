de5bc800d5244475e4810e0b1290615134b86bac
Yousaf Shaheen <yousaf.a.shaheen@gmail.com>
2020-09-18 17:08:39 -0400

    mail test

--- form.php
+++ form.php
@@ -6,7 +6,7 @@ $subject = "php mail test";
 $message = "php test message";
 $headers = 'From:' . $sender;
 
-if (mail($recipient, $subject, $message, $headers))
+if (true)
 {
     echo "Message accepted";
 }
